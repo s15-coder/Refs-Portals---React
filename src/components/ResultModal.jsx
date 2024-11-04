9b480d00ccb4f6be2d8ca4896a370004fb713aa8
Esteban Barragan Muñoz <esteban@akaunt.io>
2024-11-04 13:37:13 -0800

    feat: calculate score and use portal to render modal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,9 +1,11 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
-const ResultModal = forwardRef(({ result, targetTime, remainingTime, resetTime }, ref) => {
-    const modalRef = useRef(null);
+import { createPortal } from "react-dom";
 
+const ResultModal = forwardRef(({ targetTime, remainingTime, resetTime }, ref) => {
+    const modalRef = useRef(null);
     const userLost = remainingTime <= 0;
     const secondsLeft = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
     useImperativeHandle(ref, function () {
         return {
             open: () => {
@@ -11,15 +13,18 @@ const ResultModal = forwardRef(({ result, targetTime, remainingTime, resetTime }
             }
         }
     })
-    return <dialog ref={modalRef} className="result-modal" >
-        {userLost && <h2>You lost</h2>}
-        {!userLost && <h2>Stopped on time!</h2>}
-        <p>The target time was <strong>{targetTime} seconds.</strong></p>
-        <p>You stopped the timer with <strong>{secondsLeft} seconds left.</strong></p>
-        <form method="dialog" onSubmit={resetTime}>
-            <button>Close</button>
-        </form>
-    </dialog>
+    return createPortal(
+        <dialog ref={modalRef} className="result-modal" onClose={resetTime} >
+            {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your score: {score}</h2>}
+            <p>The target time was <strong>{targetTime} seconds.</strong></p>
+            <p>You stopped the timer with <strong>{secondsLeft} seconds left.</strong></p>
+            <form method="dialog" onSubmit={resetTime}>
+                <button>Close</button>
+            </form>
+        </dialog>,
+        document.getElementById('modal')
+    )
 })
 
 export default ResultModal
